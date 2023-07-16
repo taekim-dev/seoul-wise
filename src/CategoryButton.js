e43c079349a5d2c10d677ea2cf63521da7850b38
Tae Kim <taekim.dev@gmail.com>
2023-07-17 01:10:12 +0800

    edit header

--- src/CategoryButton.js
+++ src/CategoryButton.js
@@ -4,7 +4,7 @@ const CategoryButton = ({ title }) => {
   return (
     <div className="bg-white text-gray-800 border-gray-300 border-2 shadow-lg py-6 px-4 rounded-lg m-2 sm:w-60 sm:h-60 w-full h-full flex flex-col justify-center items-center text-center cursor-pointer mx-2">
       <img src={`images/${title}.jpg`} alt={`${title} icon`} className="h-24 w-24 object-contain mb-4"/>
-      <p className="text-lg font-semibold">{title}</p>
+      <p className="text-xl font-semibold">{title}</p>
     </div>
   );
 };
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,10 @@
 const Header = () => {
     return (
-      <header className="text-center py-6 font-bold text-2xl">
-        SeoulWise
+      <header className="text-center py-6 font-bold text-2xl text-gray-900 font-roboto">
+        Heading to Korea for...
       </header>
     );
   };
   
   export default Header;
+  
