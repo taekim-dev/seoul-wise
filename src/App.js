87f8534d2695af420b22add11c1692268ce4e660
Tae Kim <taekim.dev@gmail.com>
2023-07-16 23:00:57 +0800

    added tailwind

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1>Welcome to SeoulWise</h1>
+      <h1 className="text-center py-6 font-bold text-2xl">Welcome to SeoulWise</h1>
     </div>
   );
 }
