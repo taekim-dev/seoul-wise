f754f7ded7f3bbf0ed5daf78bcdc2459fcd85225
Tae Kim <taekim.dev@gmail.com>
2023-07-17 00:07:13 +0800

    added category icons

--- src/CategoryButton.js
+++ src/CategoryButton.js
@@ -1,14 +1,12 @@
-const CategoryButton = ({ title, icon }) => {
-    return (
-      <div className="bg-white text-gray-800 border-gray-300 border-2 shadow-lg py-4 px-2 rounded-lg m-2 w-full sm:w-60 h-48 sm:h-60 flex flex-col justify-center items-center text-center cursor-pointer">
-        <div className="mb-2">
-          {/* Placeholder for icon */}
-          <span className="text-5xl">{icon}</span>
-        </div>
-        <p className="text-lg font-semibold">{title}</p>
-      </div>
-    );
-  };
-  
-  export default CategoryButton;
-  
+import React from 'react';
+
+const CategoryButton = ({ title }) => {
+  return (
+    <div className="bg-white text-gray-800 border-gray-300 border-2 shadow-lg py-6 px-4 rounded-lg m-4 w-60 h-60 flex flex-col justify-center items-center text-center cursor-pointer">
+      <img src={`images/${title}.jpg`} alt={`${title} icon`} className="h-24 w-24 object-contain mb-4"/>
+      <p className="text-lg font-semibold">{title}</p>
+    </div>
+  );
+};
+
+export default CategoryButton;
--- src/CategoryGrid.js
+++ src/CategoryGrid.js
@@ -2,19 +2,19 @@ import CategoryButton from './CategoryButton';
 
 const CategoryGrid = () => {
   const categories = [
-    { title: 'Food & Dining', icon: '🍔' },
-    { title: 'Nature & Outdoors', icon: '🌲' },
-    { title: 'Art & Culture', icon: '🎨' },
-    { title: 'History & Heritage', icon: '🏰' },
-    { title: 'Shopping & Fashion', icon: '👠' },
-    { title: 'Adventure & Recreation', icon: '🏄‍♂️' },
+    'Food',
+    'Nature',
+    'Culture',
+    'History',
+    'Fashion',
+    'Wellness',
   ];
 
   return (
     <div className="flex justify-center px-4 sm:px-0">
       <div className="grid grid-cols-2 gap-2 sm:gap-4 place-items-center max-w-screen-sm">
-        {categories.map((category, index) => (
-          <CategoryButton key={index} title={category.title} icon={category.icon} />
+        {categories.map((title, index) => (
+          <CategoryButton key={index} title={title} />
         ))}
       </div>
     </div>
