a0d536f404886090804cf282b2bfefaf61d7694e
Tae Kim <taekim.dev@gmail.com>
2023-07-17 01:51:10 +0800

    reorganize folders

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import Home from './Home';
-import Category from './Category';
+import Home from './pages/Home';
+import Category from './pages/Category';
 
 const AppRoutes = () => {
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './Header';
-import CategoryGrid from './CategoryGrid';
-import Footer from './Footer';
+import Header from '../components/Header';
+import CategoryGrid from '../components/CategoryGrid';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
