c2957312fe2c7b4b124522a3968b1679d43c4cb0
Tae Kim <taekim.dev@gmail.com>
2023-07-17 00:32:40 +0800

    category card mobile layout

--- src/CategoryButton.js
+++ src/CategoryButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CategoryButton = ({ title }) => {
   return (
-    <div className="bg-white text-gray-800 border-gray-300 border-2 shadow-lg py-6 px-4 rounded-lg m-4 w-60 h-60 flex flex-col justify-center items-center text-center cursor-pointer">
+    <div className="bg-white text-gray-800 border-gray-300 border-2 shadow-lg py-6 px-4 rounded-lg m-4 sm:w-60 w-full sm:h-60 h-auto flex flex-col justify-center items-center text-center cursor-pointer mx-2">
       <img src={`images/${title}.jpg`} alt={`${title} icon`} className="h-24 w-24 object-contain mb-4"/>
       <p className="text-lg font-semibold">{title}</p>
     </div>
