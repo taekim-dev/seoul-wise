df05fed172030076a30eb2adf5add141dd1c7546
Tae Kim <taekim.dev@gmail.com>
2023-07-16 23:24:38 +0800

    movile adjustment

--- src/CategoryButton.js
+++ src/CategoryButton.js
@@ -1,9 +1,9 @@
 const CategoryButton = ({ title, icon }) => {
     return (
-      <div className="bg-white text-gray-800 border-gray-300 border-2 shadow-lg py-6 px-4 rounded-lg m-4 md:m-2 w-full md:w-60 h-60 flex flex-col justify-center items-center text-center cursor-pointer">
-        <div className="mb-4">
+      <div className="bg-white text-gray-800 border-gray-300 border-2 shadow-lg py-4 px-2 rounded-lg m-2 w-full sm:w-60 h-48 sm:h-60 flex flex-col justify-center items-center text-center cursor-pointer">
+        <div className="mb-2">
           {/* Placeholder for icon */}
-          <span className="text-6xl">{icon}</span>
+          <span className="text-5xl">{icon}</span>
         </div>
         <p className="text-lg font-semibold">{title}</p>
       </div>
