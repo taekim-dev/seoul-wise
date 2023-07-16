251c00e8917bd765a5bad592db8291746c18a065
Tae Kim <taekim.dev@gmail.com>
2023-07-17 00:29:19 +0800

    reset the layout

--- src/CategoryButton.js
+++ src/CategoryButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CategoryButton = ({ title }) => {
   return (
-    <div style={{aspectRatio: "1"}} className="bg-white text-gray-800 border-gray-300 border-2 shadow-lg py-4 px-2 rounded-lg m-2 w-11/12 sm:w-60 flex flex-col justify-center items-center text-center cursor-pointer">
+    <div className="bg-white text-gray-800 border-gray-300 border-2 shadow-lg py-6 px-4 rounded-lg m-4 w-60 h-60 flex flex-col justify-center items-center text-center cursor-pointer">
       <img src={`images/${title}.jpg`} alt={`${title} icon`} className="h-24 w-24 object-contain mb-4"/>
       <p className="text-lg font-semibold">{title}</p>
     </div>
