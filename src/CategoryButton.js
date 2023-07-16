a48bf7b42045c5a0a8daefdb169ab121825bd8fd
Tae Kim <taekim.dev@gmail.com>
2023-07-17 00:57:13 +0800

    mobile margin test

--- src/CategoryButton.js
+++ src/CategoryButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CategoryButton = ({ title }) => {
   return (
-    <div className="bg-white text-gray-800 border-gray-300 border-2 shadow-lg py-6 px-4 rounded-lg m-1 sm:m-2 sm:w-60 sm:h-60 w-3/4 h-3/4 flex flex-col justify-center items-center text-center cursor-pointer mx-1 sm:mx-2">
+    <div className="bg-white text-gray-800 border-gray-300 border-2 shadow-lg py-6 px-4 rounded-lg m-2 sm:w-60 sm:h-60 w-full h-full flex flex-col justify-center items-center text-center cursor-pointer mx-2">
       <img src={`images/${title}.jpg`} alt={`${title} icon`} className="h-24 w-24 object-contain mb-4"/>
       <p className="text-lg font-semibold">{title}</p>
     </div>
