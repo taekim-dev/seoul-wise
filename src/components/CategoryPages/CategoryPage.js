57d44ca336256d4feb2188243013cd9238817ace
Tae Kim <taekim.dev@gmail.com>
2023-07-17 12:04:09 +0800

    category page test

--- src/components/CategoryPages/CategoryPage.js
+++ src/components/CategoryPages/CategoryPage.js
@@ -6,9 +6,9 @@ const CategoryPage = ({ category, items }) => {
       <h1 className="font-bold text-2xl mb-4 text-center">Top 10 in {category}</h1>
       <ul className="grid grid-cols-1 gap-4 place-items-center w-full px-2 md:px-0">
         {items.map((item, index) => (
-          <li key={index} className="mb-2 bg-white text-gray-800 shadow-lg py-6 px-4 rounded-lg m-2 w-full md:w-2/5 flex flex-col justify-center items-center text-center cursor-pointer mx-2 overflow-hidden">
-            <img src={`images/${item}.jpg`} alt={`${item}`} className="h-24 w-full object-cover object-center"/>
-            <p className="text-xl font-bold mt-4">{item}</p>
+          <li key={index} className="mb-2 bg-white text-gray-800 shadow-lg rounded-lg w-full md:w-2/5 flex flex-col justify-center items-center text-center cursor-pointer overflow-hidden flex md:flex-row">
+            <img src={`images/${item}.jpg`} alt={`${item}`} className="h-full md:w-3/5 object-cover md:mr-0"/>
+            <div className="text-xl font-bold md:w-2/5 flex items-center justify-center md:ml-0">{item}</div>
           </li>
         ))}
       </ul>
