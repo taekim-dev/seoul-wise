e796db6504fc5c0aaabf2d2659ab5206e23c3e74
Tae Kim <taekim.dev@gmail.com>
2023-07-17 11:03:39 +0800

    button link added

--- src/components/CategoryButton.js
+++ src/components/CategoryButton.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CategoryButton = ({ title }) => {
   return (
-    <div className="bg-white text-gray-800 border-gray-300 border-2 shadow-lg py-6 px-4 rounded-lg m-2 sm:w-60 sm:h-60 w-full h-full flex flex-col justify-center items-center text-center cursor-pointer mx-2">
-      <img src={`images/${title}.jpg`} alt={`${title} icon`} className="h-24 w-24 object-contain mb-4"/>
-      <p className="text-xl font-semibold">{title}</p>
-    </div>
+    <Link to={`/${title.toLowerCase()}`} className="w-full text-center">
+      <div className="bg-white text-gray-800 border-gray-300 border-2 shadow-lg py-6 px-4 rounded-lg m-2 sm:w-60 sm:h-60 w-full h-full flex flex-col justify-center items-center text-center cursor-pointer mx-2">
+        <img src={`images/${title}.jpg`} alt={`${title} icon`} className="h-24 w-24 object-contain mb-4"/>
+        <p className="text-xl font-semibold">{title}</p>
+      </div>
+    </Link>
   );
 };
 
